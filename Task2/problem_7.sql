42266c8ec1123184040b21d7b6c95b940a434017
janaahmed59 <ja8040637@gmail.com>
2026-01-16 17:19:19 +0200

    Fix SQL query to use concat for full name

--- Task2/problem_7.sql
+++ Task2/problem_7.sql
@@ -1,5 +1,6 @@
 --problem 7
-select customers.fname ||' '|| customers.lname  ass ffull_name,order_id,amount
+select concat(customers.first_name, customers.last_name)  as full_name,order_id,amount
 from customers full outer join orders
 on orders.customer_id = customers.customer_id
+
 
