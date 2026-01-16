9d55383e658bdfc76b46a3535375a7a87378d498
janaahmed59 <ja8040637@gmail.com>
2026-01-16 17:20:56 +0200

    Fix SQL query formatting and add comments

--- Task2/problem_7.sql
+++ Task2/problem_7.sql
@@ -1,6 +1,7 @@
 --problem 7
-select concat(customers.first_name, customers.last_name)  as full_name,order_id,amount
+select concat(customers.first_name,' ',customers.last_name)  as full_name,order_id,amount
 from customers full outer join orders
 on orders.customer_id = customers.customer_id
+
 
 
